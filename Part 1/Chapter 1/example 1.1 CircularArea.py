4b369bcd9800dd663f01875f0cfda0debc3354b5
MineSelf2016 <1160131684@qq.com>
2019-09-04 11:28:19 +0800

    第一章 进入Python的世界

---
+++ Part 1/Chapter 1/example 1.1 CircularArea.py
@@ -0,0 +1,11 @@
+"""
+输入：圆半径raduis
+处理：计算圆面积area = π * radius * radius （此处，π取3.1415）
+输出：圆面积area 
+"""
+
+radius = float(input("Please input the radius \n"))
+
+
+# print(3.1415 * radius * radius)
+print(3.1415 * radius ** 2)
